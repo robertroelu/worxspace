b69526b7a21e79f8790bcc6955406ad380054f18
Róbert Klimant <roboklimant@gmail.com>
2024-10-21 15:51:09 +0200

    fix: sticky scroll to

--- src/utils/stickySection.ts
+++ src/utils/stickySection.ts
@@ -2,7 +2,22 @@ export const stickySection = () => {
   const stickyEl = document.querySelector('[sticky-section]') as HTMLElement;
   if (!stickyEl) return;
 
+  const elScrollFrom = document.querySelectorAll('[scroll-to-partners]') as NodeListOf<HTMLElement>;
+  if (!elScrollFrom) return;
+
+  const position = stickyEl.getBoundingClientRect().top + window.scrollY;
+
+  elScrollFrom.forEach((el) => {
+    el.addEventListener('click', () => {
+      window.scrollTo({
+        top: position,
+        behavior: 'smooth',
+      });
+    });
+  });
+
   function resize() {
+    // Code for sticky position to keep visibility
     const windowHeight = window.innerHeight;
 
     const heightEl = stickyEl.offsetHeight;
