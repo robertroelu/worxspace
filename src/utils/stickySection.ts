ec3264319cd44cded03aaa698158722c6a5f0432
Róbert Klimant <roboklimant@gmail.com>
2024-10-24 14:38:42 +0200

    fix: update

--- src/utils/stickySection.ts
+++ src/utils/stickySection.ts
@@ -1,43 +1,3 @@
-// export const stickySection = () => {
-//   const stickyEl = document.querySelector('[sticky-section]') as HTMLElement;
-//   if (!stickyEl) return;
-
-//   const elScrollFrom = document.querySelectorAll('[scroll-to-partners]') as NodeListOf<HTMLElement>;
-//   if (!elScrollFrom) return;
-
-//   let position: number;
-
-//   elScrollFrom.forEach((el) => {
-//     el.addEventListener('click', () => {
-//       window.scrollTo({
-//         top: position,
-//         behavior: 'smooth',
-//       });
-//     });
-//   });
-
-//   function resize() {
-//     // Code for sticky position to keep visibility
-//     const windowHeight = window.innerHeight;
-
-//     const heightEl = stickyEl.offsetHeight;
-
-//     const diff = windowHeight - heightEl;
-
-//     if (diff < 0) {
-//       stickyEl.style.top = `${diff}px`;
-//     }
-
-//     position = stickyEl.getBoundingClientRect().top + window.scrollY;
-//   }
-
-//   window.addEventListener('resize', () => {
-//     resize();
-//   });
-
-//   resize();
-// };
-
 export const stickySection = () => {
   const stickyEl = document.querySelector('[sticky-section]') as HTMLElement;
   if (!stickyEl) return;
@@ -46,20 +6,6 @@ export const stickySection = () => {
   if (!elScrollFrom) return;
 
   let position: number;
-
-  // Cache window height on load and during resize
-  let windowHeight = window.innerHeight;
-
-  // Throttle resize event for better performance
-  let resizeTimeout: number | undefined;
-  const throttleResize = () => {
-    if (!resizeTimeout) {
-      resizeTimeout = window.setTimeout(() => {
-        resize();
-        resizeTimeout = undefined;
-      }, 100); // Adjust throttle interval as needed
-    }
-  };
 
   elScrollFrom.forEach((el) => {
     el.addEventListener('click', () => {
@@ -71,22 +17,25 @@ export const stickySection = () => {
   });
 
   function resize() {
+    // Code for sticky position to keep visibility
+    const windowHeight = window.innerHeight;
+
     const heightEl = stickyEl.offsetHeight;
+
     const diff = windowHeight - heightEl;
 
     if (diff < 0) {
       stickyEl.style.top = `${diff}px`;
-    } else {
-      stickyEl.style.top = ''; // Reset top if no offset is needed
     }
 
     position = stickyEl.getBoundingClientRect().top + window.scrollY;
   }
 
   window.addEventListener('resize', () => {
-    windowHeight = window.innerHeight;
-    throttleResize();
+    resize();
   });
 
-  resize();
+  setTimeout(() => {
+    resize();
+  }, 500);
 };
